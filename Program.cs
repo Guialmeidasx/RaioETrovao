58b9714748fa9e2fcf16a2a761b013b39f6a4d16
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-05-24 22:24:07 -0300

    executado com sucesso

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("--- Raio e Trovão ---\n");
+
+Console.WriteLine($"trovão a {3} segundos ==> raio atingiu {DistanciaDoRaioKm(3):N5} quilômetros");
+Console.WriteLine($"trovão a {6} segundos ==> raio atingiu {DistanciaDoRaioKm(6):N5} quilômetros");
+
+double DistanciaDoRaioKm(double segundos)
+{
+    const double VelocidadeDoSomKmS = 340.29 / 1000;
+    return segundos * VelocidadeDoSomKmS;
+}
